79c6b7bf01a40a8b0d143e0e7a8abb9687318cdd
william.baker <william.baker@MU-C3-02.mullen.pvt.k12.co.us>
2015-05-22 11:24:03 -0600

    initial commit

---
+++ Final Project/src/PongMain.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class PongMain 
+{
+	public static void main(String[] args)
+	{
+
+
+	}
+
+}
---
+++ Final Project/src/setGameSize.java
@@ -0,0 +1,11 @@
+
+public class setGameSize 
+{
+
+	public static void main(String[] args) 
+	{
+	System.out.println("Let's play a game of pong!");	
+
+	}
+
+}
